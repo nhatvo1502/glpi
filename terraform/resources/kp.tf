d017c84693e6193a24c2ba4e78548b49d2e06c6b
nhat <kevin@Supports-MacBook-Pro-2.local>
2025-12-16 09:01:59 -0800

    keypair

--- terraform/resources/kp.tf
+++ terraform/resources/kp.tf
@@ -1,4 +1,9 @@
-resource "aws_key_pair" "glpi_ec2_kp" {
-    key_name = "GLPI EC2 Key"
-    public_key = 
+resource "tls_private_key" "local_private_key" {
+  algorithm   = "RSA"
+  rsa_bits = 4069
+}
+
+resource "aws_key_pair" "glpi_key" {
+    key_name = "glpi_key"
+    public_key = tls_private_key.local_private_key.public_key_openssh
 }
