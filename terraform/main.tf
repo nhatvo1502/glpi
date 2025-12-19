6719bc88358b21c6abaf2f4870cba9fbeb914f74
nhat <kevin@Supports-MacBook-Pro-2.local>
2025-12-18 16:03:07 -0800

    update action

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 module "network" {
-    source = "./network"
+    source = "./modules/network"
 }
 
 module "resources" {
-    source = "./resources"
+    source = "./modules/resources"
     vpc_id = module.network.vpc_id
     subnet_id = module.network.subnet_id
 }
